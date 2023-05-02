a072ae26094345f612c9b36d8469343f3fa05979
lysgut <lucas77tuninetti@gmail.com>
2023-05-02 00:10:46 -0300

    lo tengo más que dominado, gracias hector

--- index.js
+++ index.js
@@ -7,26 +7,39 @@ const express = require("express")
 const app = express()
 const mongoose = require("mongoose")
 const urlMongoose = `mongodb+srv://lysgut:Mongoatlas177@cluster0.yvzut0t.mongodb.net/?retryWrites=true&w=majority`
-const port = 2005
+const port = 20005
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
-mongoose.Promise = global.Promise; // Utilddizar promesas nativas de Node.js
 mongoose.connect(urlMongoose, { useNewUrlParser: true })
-  .then(() => console.log('Conexión exitosa a la base de datos'))
-  .catch((err) => console.error(err));
+
+mongoose.connection.on('connected', function () {
+  console.log('Conexión exitosa a la base de datos')
 
   const Clan = require('./models/clanes'); // Reemplaza "clan" con el nombre de tu modelo
 
+
   Clan.find({})
-    .then((docs) => {
-      console.log("Los siguientes clanes se encuentran en la base de datos:");
-      console.log(docs);
-    })
-    .catch((err) => {
-      console.error(err);
-    });
+  .then((docs) => {
+    console.log("Los siguientes clanes se encuentran en la base de datos:");
+  const IntegrantesNombres = docs.map((clan) => "Integrantes Generacion 2 del clan:" + clan.nombre + ": " + clan.generaciones.gen2.integrantes);
+  const IntegrantesCantidad = docs.map((clan) => "Cantidad de integrantes de la Generacion 2 del clan " + clan.nombre+ ": " + clan.generaciones.gen2.integrantes.length)
+    console.log(IntegrantesNombres);
+    console.log(IntegrantesCantidad);
+  })
+  .catch((err) => {
+    console.error(err);
+  });
+
+  }
+);
+
+
+
+
 app.listen(port, () => {
     console.log("Servidor iniciado en el puerto " + port);
 });
+
+
 
